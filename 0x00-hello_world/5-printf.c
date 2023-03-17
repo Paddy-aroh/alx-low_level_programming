ec531c3db5546a23efe5e189276a9c3ccf262f48
Paddy-aroh <louisswavey@gmail.com>
2023-03-16 20:15:07 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints with proper grammar, but the outcome is a piece of art,
+* Return ^[^[[1;2D^Z
+[3]+  Stopped                 cat > 5-printf.c
+root@fff258777487:/alx-low_level_programming/0x00-hello_world# cat > 5-printf.c
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: usu^[[D^Z
+[2]+  Stopped                 cat > 5-printf.c
+root@fff258777487:/alx-low_level_programming/0x00-hello_world# cat > 5-printf.c
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
