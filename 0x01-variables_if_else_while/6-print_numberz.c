840e89fdb84b4586a24aa04b64c91de1a36911b1
Paddy-aroh <louisswavey@gmail.com>
2023-03-18 02:09:22 -0700

    numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints numbers from 0 to 9
+*
+* Description: using the main function
+* this program prints "Programming of numbers from 0 to 9
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
