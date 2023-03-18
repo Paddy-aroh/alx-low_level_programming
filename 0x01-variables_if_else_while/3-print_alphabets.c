34175ab418af88ca79a038be27d475d77418a12e
Paddy-aroh <louisswavey@gmail.com>
2023-03-17 20:05:38 -0700

    if the  alphabet is in lowercase and then in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - print if the  alphabet is in lowercase and then in uppercase
+*
+* Description: using the main function
+* this program prints "Programming if the  alphabet is in lowercase and then in uppercase
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
