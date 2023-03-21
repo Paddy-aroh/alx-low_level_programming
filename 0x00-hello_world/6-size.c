caaed3b2b88420d1ac19029ab9a078ec72a78477
Paddy-aroh <louisswavey@gmail.com>
2023-03-20 19:43:25 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,15 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
-* You should produce the exact output as in the example
-* Warnings are allowed
+* main - Entry point
 *
-* Your program should return 0
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-char c;
-int i;
-long li;
-long long lli;
-float f;
-printf("size of a chart: %ld byte(s)\n", sizeof(c));
-printf("size of a int: %ld byte(s)\n", sizeof(i));
-printf("size of a long: %ld byte(s)\n", sizeof(li));
-printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-printf("size of a float: %ld byte(s)\n", sizeof(f));
-return (0);
-}
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}	
